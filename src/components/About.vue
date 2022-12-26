62f5a59e65bb2890cc53fef9a0bdfead8464d41f
Blessing Boaslah <blessing@kwagei.com>
2022-12-26 13:38:08 +0000

    updated about

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,19 +6,20 @@
         </div>
         <div>
             <p>
-                Hi, welcome to my corner, I'm Blessing Boaslah. I'm a christian, a life learner and Software Engineer working at 
+                Hi, welcome to my corner. I'm Blessing Boaslah, a Christian, a life learner, and a Liberian Software Engineer working at 
                 <a href="https://kwagei.com">Kwagei Group</a>.</p>
-                <p>I currently live in Liberia, where I've spent all my years so far.</p>
-                <p>As a Software Engineer, I'm from the JavaScript background, and I'm currently learning Vue.js amongst others. </p>
-                <p>I built this blog with HTML, CSS and Vue.js.  Moreover, I'm doing other personal projects in vue, let's see how that goes.</p>
+                <p>I currently live in Liberia, this sweet land of liberty, where I've spent all my years so far.</p>
+                
+                <p> As a Software Engineer, I'm from 'the JavaScript' background, and I'm currently learning Vue.js amongst others.
+                    I built this blog with HTML, CSS, Vue.js and a bit of Node.js. Moreover, I'm doing other personal projects in Vue.js.</p>
 
-                <p>I started making tech videos for YouTube early this year, because I found out that no one does that in Liberia, at least for now. I upload videos like once or twice every month
-                 because of how busy I can be. Obviously, I'm learning to improve on that.</p>
+                <p>I started making tech videos for YouTube late 2021, because I found out no one does that in Liberia, at least for now. 
+                    It's also my goal to share my journey and knowledge with young Liberians who want to become Software Engineers. </p>
 
-                <p>This blog is the result of my constant habit of journaling. Over the years, I've grown to love writing down my thoughts and experiences.</p>
-                <p>Hence, I've decided to explore the public writing space, and of course, I'm open to corrections and feedback. I'd happily embrace them.</p>
+                <p>This blog results from my constant habit of journaling. Over the years, I've grown to love writing my thoughts and experiences.   
+                    Hence, I've decided to explore the public writing space, and of course, I'm open to corrections and feedback. I'd happily embrace them.</p>
 
-                <p>Thanks for stopping by. xoxo</p>
+                <p>Thanks for stopping by, do have a lovely day. xoxo</p>
                 <p>- Blessing &hearts;
             </p>
         </div>
